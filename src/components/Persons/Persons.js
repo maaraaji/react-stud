75b70e86569b670b3f8e05e4ddc5500fea68686d
maaraa <maaraa@dotworld.in>
2018-01-27 20:04:17 +1100

    Coverted Persons component into Class based Stateful component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map( (p, index) => {
-  return <Person
-    personName={p.name}
-    personAge={p.age}
-    key={p.id}
-    click={() => props.clicked (index)}
-    changeName={(event) => props.changed (event, p.id)}
-    ></Person>
-});
+class Persons extends Component {
+  render () {
+    return this.props.persons.map( (p, index) => {
+      return <Person
+        personName={p.name}
+        personAge={p.age}
+        key={p.id}
+        click={() => this.props.clicked (index)}
+        changeName={(event) => this.props.changed (event, p.id)}
+        ></Person>
+    });
+  }
+}
 
-
-export default persons
+export default Persons
