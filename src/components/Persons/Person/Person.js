61d7274b6eb54dea6833c3b0d32957579aa53a62
maaraa <maaraa@dotworld.in>
2018-01-27 20:17:53 +1100

    Retaining Stateless component methods to see what changed for future reference

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Styles from './Person.css';
 
-const person = (props) => {
-  return (
-    <div className={Styles.Person}>
-      <p onClick={props.click}>I am {props.personName} and I am {props.personAge} years old</p>
-      <input type='text' onChange={props.changeName} value={props.personName}/>
+class Person extends Component {
+  render () {
+    return <div
+      className={Styles.Person}>
+      <p onClick={this.props.click}>I am {this.props.personName} and I am {this.props.personAge} years old</p>
+      <input type='text' onChange={this.props.changeName} value={this.props.personName}/>
     </div>
-  )
-};
+  }
+}
 
-export default person;
+// const person = (props) => {
+//   return (
+//     <div className={Styles.Person}>
+//       <p onClick={props.click}>I am {props.personName} and I am {props.personAge} years old</p>
+//       <input type='text' onChange={props.changeName} value={props.personName}/>
+//     </div>
+//   )
+// };
+// export default person;
+
+export default Person
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,16 +4,26 @@ import Person from './Person/Person';
 
 class Persons extends Component {
   render () {
-    return this.props.persons.map( (p, index) => {
-      return <Person
+    return this.props.persons.map( (p, index) => <Person
         personName={p.name}
         personAge={p.age}
         key={p.id}
         click={() => this.props.clicked (index)}
         changeName={(event) => this.props.changed (event, p.id)}
         ></Person>
-    });
+    );
   }
 }
+
+// const persons = (props) => props.persons.map( (p, index) => {
+//   return <Person
+//     personName={p.name}
+//     personAge={p.age}
+//     key={p.id}
+//     click={() => props.clicked (index)}
+//     changeName={(event) => props.changed (event, p.id)}
+//     ></Person>
+// });
+// export default persons
 
 export default Persons
