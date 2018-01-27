cce19ffb242f6f2e85171f05bad53859f61b681d
maaraa <maaraa@dotworld.in>
2018-01-28 00:40:49 +1100

    [InAction Explained] Using HOC Aux Component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Styles from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
   const classes = []
-  let buttonClass = ''
+  let buttonClass = Styles.Button
 
   if (props.showPersons){
-    buttonClass = Styles.showPersons
+    buttonClass = [Styles.Button, Styles.showPersons].join(' ')
   }
 
   if (props.persons.length <=2) {
@@ -18,13 +19,15 @@ const cockpit = (props) => {
   }
 
   return (
-    <div className={Styles.Cockpit}>
-      <h1>
-        ReactJS Study Guide - The descriptions are enclosed in source codes
-      </h1>
-      <p className={classes.join(' ')}>Checking Dynamic Classes</p>
-      <button className={buttonClass} onClick={props.clicked}>TOGGLE NAME CARD</button>
-    </div>
+    // <div className={Styles.Cockpit}>     //Instead of using Div (Wrapping enclosing tag) use Aux HOC if you are using React 16
+      <Aux>                                 //If you are using React 16.2 no need to use Aux by defining Aus HOC component instead React does the job so simple enclose with <></>
+        <h1>
+          ReactJS Study Guide - The descriptions are enclosed in source codes
+          </h1>
+          <p className={classes.join(' ')}>Checking Dynamic Classes</p>
+          <button className={buttonClass} onClick={props.clicked}>TOGGLE NAME CARD</button>
+      </Aux>
+    // </div>
   );
 };
 
